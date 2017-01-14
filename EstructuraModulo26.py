40fa9fa4e1ecede48362b68894e8a9e7ac9df2e0
Luis Eduardo Rosero Correa <zerhiphop@live.com>
2017-01-14 13:33:24 -0500

    Subido via upload
    Creacion del archivo EstructuraModulo26.py y modificacion del archivo Calculadora.py para indicar cual debe ser la estructura que deben presentar los modulos

---
+++ EstructuraModulo26.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Se modifica este archivo para indicar como se debe manejar los modulos en cuanto a su estructura
+además de añadir los metadatos que se indican a continuación
+"""
+__author__="Luis Eduardo Rosero Correa"
+__copyright__="Copyright 2016"
+__credits__="Eduardo Rosero"
+__licence__="GPL"
+__version__="1.0"
+__maintainer__="Eduardo Rosero"
+__email__="zerhiphop@live.com"
+__status__="Developer"
+
+def suma(num_uno, num_dos):
+	"""Recibe dos numeros como parametros y devuelve la suma de los mismos"""
+	return num_uno+num_dos
+
+def resta(num_uno, num_dos):
+	"""Recibe dos numeros como parametros y devuelve la diferencia de los mismos"""
+	return num_uno-num_dos
+
+def multiplicacion(num_uno, num_dos):
+	"""Recibe dos numeros como parametros y devuelve la multiplicacion de los mismos"""
+	return num_uno*num_dos
+
+def division(num_uno, num_dos):
+	"""Recibe dos numeros como parametros y devuelve la division de los mismos"""
+	return num_uno/num_dos
