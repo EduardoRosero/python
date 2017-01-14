7bea9b1a84f81ebeea151aca660b830ec6f5a2d0
Luis Eduardo Rosero Correa <zerhiphop@live.com>
2017-01-14 13:43:24 -0500

    Subido via upload
    Creacion del archivo Name27.py que nos indica un el valor por defecto del metadato __name__ de los archivos y como podemos utilizarlo dentro del mismo archivo y en las importaciones de otros archivos

---
+++ Name27.py
@@ -0,0 +1,6 @@
+from Calculadora import __name__ as __name__calculadora__
+
+print(__name__)
+print(__name__calculadora__)
+
+#__name__ tiene el atributo main cuando el archivo en el que trabajamos es el principal
