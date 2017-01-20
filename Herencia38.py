2ac5c0167088ae95934071371795f13b196d60dd
Eduardo <zerhiphop@live.com>
2017-01-19 23:14:16 -0500

    Modificacion del archivo Herencia.py con la creacion de las clases Animal, Felino con sus respectivos metodos de tal manera que Felino herede de Animal y Gato y Jaguar hereden de Felino

--- Herencia38.py
+++ Herencia38.py
@@ -1,23 +1,28 @@
 #La herencia nos permite crear clases a partir de otras clases
+class Animal:
+	def terrestre(self):
+		return True
 
-class Gato:
+class Felino(Animal): #Clase padre de la que se van a heredar metodos y atributos
 	@property
 	def garras_retractiles(self):
 		return True
 
+		"""Los metodos privador __cazar() no se pueden heredar en las clases hijos"""
 	def cazar(self):
 		print("El felino esta cazando")
 
-class Jaguar:
-	@property
-	def garras_retractiles(self):
-		return True
+class Gato(Felino): #Entre parentesis colocamos el nombre de la clase que queremos heredar
+	def gato_cazador(self):
+		self.cazar()
 
-	def cazar(self):
-		print("El felino esta cazando")
+class Jaguar(Felino):
+	pass
 
 gato = Gato()
 jaguar = Jaguar()
 
-print(gato.garras_retractiles)
+print(gato.terrestre())
+print(jaguar.terrestre())
 print(jaguar.garras_retractiles)
+
