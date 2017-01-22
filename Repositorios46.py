a1abdc2106f594fea73ce3c245a344502737a772
Eduardo <zerhiphop@live.com>
2017-01-22 12:01:46 -0500

    Creacion del archivo repositorios45.py en donde se indica los repositorios de python que se pueden visitar y como realizar instalacion de paquetes

---
+++ Repositorios46.py
@@ -0,0 +1,19 @@
+"""PyPI es un repositorio en donde se puede sibir y buscar paquetes
+relacionados con casi cualquier tema, que los poemos descargar 
+y utilizar para lo que necesitemos si se acopla a nuestras
+necesidades.
+
+para usar paquetes:
+
+easy_install
+pip install 
+
+los dos van a buscar en el repositorio y lo van a instalar"""
+
+ejemplo: en la terminal escribir
+pip install dedec
+
+
+se puede instalar virtualenv para crear entornos virtuales
+
+es decir carpetas de proyectos para cada proyecto
