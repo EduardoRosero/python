2dd03d7abbd1e411f0e3d826ba314c27cec83e6d
Eduardo <zerhiphop@live.com>
2017-01-24 00:02:21 -0500

    Creacion del archivo cadenas3.rb

---
+++ curso ruby/cadenas3.rb
@@ -0,0 +1,20 @@
+cadena = "Hola"
+
+cadena2 = 'Mundo'
+
+#Concatenacion
+
+conc = cadena + cadena2
+puts conc
+
+#Interpolacion
+inter ="Hola #{cadena2.upcase}"
+puts inter
+
+inter ="Hola #{cadena2.downcase}"
+puts inter
+
+inter ="Hola #{cadena2.capitalize}"
+puts inter
+
+puts "".methods
