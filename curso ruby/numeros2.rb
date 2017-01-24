5f520a1b2aa1f1e6b0a77157be7e408c98cd8fe2
Eduardo <zerhiphop@live.com>
2017-01-23 23:53:18 -0500

    Creacion del archivo numeros.rb en donde se indica el tipo de numeros que se pueden usar en ruby y algunas operaciones

---
+++ curso ruby/numeros2.rb
@@ -0,0 +1,27 @@
+#Se tiene los enteros y los de punto flotante o floats
+
+entero = 2
+flotante = 2.0
+resultado = entero + flotante
+puts resultado
+
+puts 10/3
+
+puts 10.0/3.0
+
+#convertir a flotante
+10.to_f
+
+#Convertir a entero
+12.5.to_i
+
+#Valor absoluto
+
+-10.abs
+2.even
+
+3.next
+
+
+
+
