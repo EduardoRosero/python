325a7e71b39601edbafe73b164269ed0b687cee2
Eduardo <zerhiphop@live.com>
2017-01-23 23:47:54 -0500

    Modificacion del archivo variables.rb para aprendea a manejar variables en ruby con ejemplos

--- curso ruby/variables.rb
+++ curso ruby/variables.rb
@@ -4,4 +4,12 @@ nombre = "Eduardo"
 
 edad = 24
 
+nombre = "Yo" #Se puede sobreescribir las variables
+
 puts nombre
+
+
+nombre_del_autor = "Eduardo"
+_nombre_del_autor = "Eduardo"
+
+puts nombre_del_autor
